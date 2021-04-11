b8708e85c52281998ffd4ecd1c3ae76c224caeb2
shi-n <shi-n_xyz@cloudsquare.jp>
2021-04-12 06:40:31 +0900

    Initial Commit

---
+++ Regular Expression Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Regular Expression Test
+//
+//  Created by Shin Inaba on 2021/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Regular Expression Test/Regular_Expression_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Regular_Expression_TestApp.swift
+//  Regular Expression Test
+//
+//  Created by Shin Inaba on 2021/04/12.
+//
+
+import SwiftUI
+
+@main
+struct Regular_Expression_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
