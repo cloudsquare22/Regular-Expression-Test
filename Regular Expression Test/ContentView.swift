f6df3c71b0817b5a81fbbc5b56421cb95d426c62
shi-n <shi-n_xyz@cloudsquare.jp>
2021-04-12 06:44:43 +0900

    init

--- Regular Expression Test/ContentView.swift
+++ Regular Expression Test/ContentView.swift
@@ -7,15 +7,71 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    var testStrings:[String] = []
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                RegularExpression()
+                Section() {
+                Text("Result")
+                TestString()
+                TestString()
+                TestString()
+                }
+            }
+            .navigationBarTitle("Test", displayMode: .inline)
+            .navigationBarItems(leading: AddButton(), trailing: EditButton())
+            .onAppear { UITableView.appearance().separatorStyle = .none }
+            .onDisappear { UITableView.appearance().separatorStyle = .singleLine }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+//            .environment(\.locale, .init(identifier: "ja"))
+    }
+}
+
+struct RegularExpression: View {
+    @State var text: String = ""
+    var body: some View {
+        HStack() {
+            Image(systemName: "square.fill")
+                .padding(8.0)
+                .foregroundColor(Color.red)
+                .scaleEffect(2)
+            TextField("Regular Expression", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+//            Button(action: {}, label: {
+//                Image(systemName: "clear")
+//            })
+        }
+    }
+}
+
+struct TestString: View {
+    @State var text: String = ""
+    var body: some View {
+        HStack() {
+            Image(systemName: "square.fill")
+                .padding(8.0)
+                .foregroundColor(Color.red)
+                .scaleEffect(2)
+            TextField("Test String", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+    }
+}
+
+struct AddButton: View {
+    var body: some View {
+        Button(action: {}, label: {
+            Image(systemName: "plus")
+        })
     }
 }
